e0a04e89e792413a4401afd8e6dae1d65bcc412b
terrellturner <terrell@rellsoft.dev>
2024-08-28 10:58:12 -0500

    Frontend setup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Logo from '/images/logo.svg'
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -9,6 +10,26 @@ const Header = () => {
           <div className="h-10 w-8 bg-white">TC4C</div>
           </div>
         <div>
+        </div>
+        <div className="hidden h-full md:flex items-center md:space-x-8 text-[#ffffff] md:mr-4 justify-center">
+          <div className="group">
+            <a href="#">Events</a>
+            <div className="group-hover:border-b group-hover:border-white w-full"></div>
+          </div>
+          <div className="group">
+            <a href="#">About</a>
+            <div className="group-hover:border-b group-hover:border-white w-full"></div>
+          </div>
+          <div className="group">
+            <FaShoppingCart className="inline-block m-1"/>
+            <a href="#">Cart</a>
+            <div className="group-hover:border-b group-hover:border-white w-full"></div>
+          </div>
+          <div className="group">
+            <FaUser className="inline-block m-1"/>
+            <a href="#">Profile</a>
+            <div className="group-hover:border-b group-hover:border-white w-full"></div>
+          </div>
         </div>
       </nav>
     </header>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import Logo from '/images/tc4c-one.svg'
+
+const events = [
+  {title: 'Wowzas!', start: new Date()}
+]
 
 const Homepage = () => {
   return (
-    <div className="min-h-full grow bg-newsletter-black flex flex-col items-center">
-    </div>
+    <div className="min-h-full grow bg-newsletter-black flex flex-col ">
+        <div className="h-full w-1/2 flex">
+          <img src={Logo} alt="" className='w-full h-full'/>
+          <img src={Logo} alt="" className='w-full h-full'/>
+        </div>
+        <div className="w-2/3 mx-auto bg-draft-yellow rounded-xl p-8">
+        <FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth' weekends={true} events={events} eventContent={renderEventContent} />
+        </div>
+   </div>
+  )
+}
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
   )
 }
 
