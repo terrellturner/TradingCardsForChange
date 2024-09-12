930b0838ba9209b61f59c3b0034ae5863436e45b
terrellturner <terrell@rellsoft.dev>
2024-09-11 19:09:53 -0500

    Connecting server to MDB, data models for users products and orders

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.js";
 import cors from "cors";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 app.use(cors());
