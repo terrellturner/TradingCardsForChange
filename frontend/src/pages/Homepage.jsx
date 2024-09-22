903ea791a016aa81fef0e8f793335bb4de8ac3c7
terrellturner <terrell@rellsoft.dev>
2024-09-21 22:20:09 -0500

    Implementing product api slice

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,40 +1,43 @@
-import React from "react";
-import {useEffect, useState} from 'react'
-import Logo from '/images/tc4c-one.svg'
-import ProductCard from '../components/ProductCard'
-import axios from 'axios';
+import React from 'react';
+import Logo from '/images/tc4c-one.svg';
+import ProductCard from '../components/ProductCard';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
+    const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('http://localhost:5000/api/products');
-      setProducts(data);
-      console.log(data);
-      
-    }
+    return (
+        <>
+            {isLoading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <div>{error.data?.message || error.error}</div>
+            ) : (
+                <>
+                    <div className="flex min-h-full grow flex-col bg-newsletter-black ">
+                        <div className="flex h-full w-1/2">
+                            <img src={Logo} alt="" className="h-full w-full" />
+                            <img src={Logo} alt="" className="h-full w-full" />
+                        </div>
+                        <div className="mx-auto my-5 grid w-4/5 max-w-[90rem] grid-cols-1 gap-10 md:grid-cols-2 2xl:grid-cols-3">
+                            {products ? (
+                                products.map((product, index = product._id) => {
+                                    return (
+                                        <ProductCard
+                                            product={product}
+                                            key={index}
+                                        />
+                                    );
+                                })
+                            ) : (
+                                <></>
+                            )}
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
+    );
+};
 
-    fetchProducts();
-  }, [])
-  
-
-  return (
-    <div className="min-h-full grow bg-newsletter-black flex flex-col ">
-        <div className="h-full w-1/2 flex">
-          <img src={Logo} alt="" className='w-full h-full'/>
-          <img src={Logo} alt="" className='w-full h-full'/>
-        </div>
-        <div className="my-5 w-4/5 mx-auto grid grid-cols-1 gap-10 md:grid-cols-2 2xl:grid-cols-3 max-w-[90rem]">
-          
-          {products ? (products.map((product, index=product._id)=>{
-            return (
-              <ProductCard product={product} key={index} />
-            )
-          })) : (<></>)}
-        </div>
-   </div>
-  )
-}
-
-export default HomePage
+export default HomePage;
