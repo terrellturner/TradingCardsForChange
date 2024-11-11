67686a2aa4fdef6f4d10e423decf5eb3907e6a49
terrellturner <terrell@rellsoft.dev>
2024-11-10 18:19:08 -0600

    Prod changes

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,11 @@
-import globals from "globals";
+import globals, { node } from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {env: {node: true}},
   {languageOptions: { globals: globals.browser, }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log(env.VITE_PROXY);
+  
+  return({plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: import.meta.env.VITE_PROXY,
-      },
+      '/api': env.VITE_PROXY
     },
-  },
+  },})
+  
 })
