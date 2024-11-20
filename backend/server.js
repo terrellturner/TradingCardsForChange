30ad9c4dff21961594c082fb16b596b72f37a358
terrellturner <terrell@rellsoft.dev>
2024-11-20 17:04:03 -0600

    CORS config

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -10,6 +11,12 @@ import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "https://trading-cards-for-change-2e2w.vercel.app/",
+};
+
+app.use(cors(corsOptions));
 
 connectDB();
 
