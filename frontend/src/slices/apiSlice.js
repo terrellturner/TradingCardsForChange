c1ea0bfe0278abc486d5fd5cbdb514f58a343791
terrellturner <terrell@rellsoft.dev>
2024-11-21 20:18:04 -0600

    Clearing cache

--- frontend/src/slices/apiSlice.js
+++
@@ -1,9 +0,0 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-
-const baseQuery = fetchBaseQuery({baseUrl: `${import.meta.env.VITE_BASE_URL}`});
-
-export const apiSlice = createApi({
-        baseQuery,
-        tagTypes:['Product','Order','User'],
-        endpoints:(builder) => ({}),
-    })
