7fe6fbb273fa1441d06eade993e2c1c33a87eeaa
terrellturner <terrell@rellsoft.dev>
2024-12-17 11:59:00 -0600

    Adding CORS back to server.js

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,12 @@ import {
   ipFilter,
 } from "./middleware/errorMiddleware.js";
 
+var cors = require("cors");
+
 const app = express();
 
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
 connectDB();
 
 //Body parser middleware
@@ -28,6 +29,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(ipFilter);
+app.use(
+  cors({
+    origin: process.env.CORS_DOMAIN,
+  }),
+);
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
