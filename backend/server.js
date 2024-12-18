557318dc4dd6ab0bcc33ba55b7613fea19b9057e
terrellturner <terrell@rellsoft.dev>
2024-12-17 20:52:13 -0600

    Reading CORS domain from .env

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: [
-      "https://trading-cards-for-change.vercel.app",
-      "https://trading-cards-for-change-rosy.vercel.app",
-    ],
+    origin: process.env.DOMAIN_WHITELIST,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
