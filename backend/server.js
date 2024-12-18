f1adeaa0fabf36fa828c6be11d51fa4bbc5e6a1b
terrellturner <terrell@rellsoft.dev>
2024-12-17 19:58:58 -0600

    Changed CORS to use .env origin domains!

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.DOMAIN_WHITELIST.split(","),
     credentials: true,
   }),
 );
