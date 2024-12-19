67238f08d37b7f06b0e10bb01a97e27206a90bd1
terrellturner <terrell@rellsoft.dev>
2024-12-19 16:16:42 -0600

    Resolving remaining CORS/JWT cookie errors

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ app.use(
     origin: [
       "https://tc4c-backend.vercel.app",
       "https://trading-cards-for-change.vercel.app",
+      "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   //Save JWT as HTTP-only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
