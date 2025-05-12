d4ce8006a6dc521414b276befc37394d40f8d39b
terrellturner <terrell@rellsoft.dev>
2025-05-11 21:32:13 -0500

    Implementing new booking routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import {
@@ -13,7 +15,6 @@ import {
   errorHandler,
   ipFilter,
 } from "./middleware/errorMiddleware.js";
-import { log } from "console";
 
 const port = process.env.PORT || 5000;
 
@@ -48,6 +49,8 @@ app.use(ipFilter);
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
