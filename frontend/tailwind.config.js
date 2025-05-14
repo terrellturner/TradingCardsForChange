634b5f2036cadc76d18c0589d51a1898d146df00
terrellturner <terrell@rellsoft.dev>
2025-05-14 15:06:58 -0500

    More rebranding changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                'hops-green': '#172601',
-                'pale-ale-green': '#687343',
-                'draft-yellow': '#D9BC66',
-                'ipa-beige': '#D9B166',
-                'newsletter-black': '#0D0D0D',
-                'off-white': '#ececec',
-                'opaque-newsletter-black': 'rgb(13,13,13,0.7)',
-            },
-        },
-        fontFamily: {
-            serif: ['Baskervville SC', 'serif'],
-            'sans-serif': ['Poppins', 'sans-serif'],
-            jacquard: ['"Jacquard 24"', 'serif'],
-            elfreth: ['elfreth', 'serif'],
-        },
-    },
-    plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'emerald-green': '#284139',
+				wasabi: '#809076',
+				'creased-khaki': '#FBD794',
+				'egyptian-earth': '#BB6830',
+				'noir-de-vigne': '#111A19',
+				'off-white': '#ececec',
+				'opaque-noir-de-vigne': 'rgb(13,13,13,0.7)',
+			},
+		},
+		fontFamily: {
+			serif: ['Playfair Display', 'serif'],
+			'sans-serif': ['Lato', 'sans-serif'],
+			jacquard: ['"Jacquard 24"', 'serif'],
+		},
+	},
+	plugins: [],
 };
