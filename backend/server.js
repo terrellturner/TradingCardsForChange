e0d9e7254b427568b3008dfd3e0ba5b9657a6d40
terrellturner <terrell@rellsoft.dev>
2025-05-14 18:13:25 -0500

    Switching CORS domains to env variables

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: [
-      "https://tradingcardsforchange-frontend.onrender.com/",
-      "http://localhost:5173",
-    ],
+    origin: process.env.CORS_DOMAINS.split(","),
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
