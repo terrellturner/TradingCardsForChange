87733ab9848cb6a474da7c3a199c9a647a6b981b
terrellturner <terrell@rellsoft.dev>
2025-05-25 20:16:54 -0500

    Removing unnecessary animatepresence wrapper

--- frontend/src/components/UI/Loader.jsx
+++ frontend/src/components/UI/Loader.jsx
@@ -27,18 +27,16 @@ const Loader = () => {
 		},
 	};
 	return (
-		<AnimatePresence>
-			<motion.div
-				key={'loader'}
-				initial="initial"
-				animate="open"
-				exit="closed"
-				variants={loaderVariants}
-				className="fixed bottom-0 left-0 right-0 top-0 flex h-full w-full items-center justify-center bg-noir-de-vigne bg-opacity-80"
-			>
-				<Logo classNames={`w-4/5 lg:flex`} />
-			</motion.div>
-		</AnimatePresence>
+		<motion.div
+			key={'loader'}
+			initial="initial"
+			animate="open"
+			exit="closed"
+			variants={loaderVariants}
+			className="fixed bottom-0 left-0 right-0 top-0 flex h-full w-full items-center justify-center bg-noir-de-vigne bg-opacity-80"
+		>
+			<Logo classNames={`w-4/5 lg:flex`} />
+		</motion.div>
 	);
 };
 
